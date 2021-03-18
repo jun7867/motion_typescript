f206e2c71778c83deb8f3a94b935d8c9e584c373
jun7867 <jjune095@naver.com>
2021-03-19 01:09:53 +0900

    Create PageItemComponent , Composable interface for reuse

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,28 @@
-import { ImageComponent } from "./components/page/item/image.js";
-import { NoteComponent } from "./components/page/item/note.js";
-import { TodoComponent } from "./components/page/item/todo.js";
-import { VideoComponent } from "./components/page/item/video.js";
-import { PageComponent } from "./components/page/page.js";
+import { Component } from './components/page/component.js';
+import { ImageComponent } from './components/page/item/image.js';
+import { NoteComponent } from './components/page/item/note.js';
+import { TodoComponent } from './components/page/item/todo.js';
+import { VideoComponent } from './components/page/item/video.js';
+import { Composable, PageComponent } from './components/page/page.js';
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable; // Component중 하나 + Composable으로 구현
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
 
-    const image = new ImageComponent(
-      "Temp Image Title",
-      "https://picsum.photos/600/300"
-    );
-    image.attachTo(appRoot, "beforeend");
+    const image = new ImageComponent('Temp Image Title', 'https://picsum.photos/600/300');
+    this.page.addChild(image);
 
-    const video = new VideoComponent(
-      "Video Title",
-      "https://youtu.be/T77T-ytINzo"
-    );
-    video.attachTo(appRoot, "beforeend");
+    const video = new VideoComponent('Video Title', 'https://youtu.be/T77T-ytINzo');
+    this.page.addChild(video);
 
-    const note = new NoteComponent("Note Title", "Note Desc");
-    note.attachTo(appRoot, "beforeend");
+    const note = new NoteComponent('Note Title', 'Note Desc');
+    this.page.addChild(note);
 
-    const todo = new TodoComponent("Todo Title", "Todo item");
-    todo.attachTo(appRoot, "beforeend");
+    const todo = new TodoComponent('Todo Title', 'Todo item');
+    this.page.addChild(todo);
   }
 }
 
 // querySelector는 null이 될 수도 있다. 하지만 확실히 존재하기 때문에 Type Assertion을 사용한다.
-new App(document.querySelector(".document")! as HTMLElement);
+new App(document.querySelector('.document')! as HTMLElement);
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,8 +1,36 @@
-import { BaseComponent } from "./component.js";
+import { BaseComponent, Component } from './component.js';
 
-export class PageComponent extends BaseComponent<HTMLUListElement> {
+export interface Composable {
+  addChild(child: Component): void;
+}
+
+export class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+  constructor() {
+    super(`<li class="page-item">
+            <section class="page-item__body"></section>
+            <div class="page-item__controls">
+                <button class="close">&times;</button>
+            </div>
+            </li>`);
+  }
+  // child를 container에 붙이기.
+  addChild(child: Component) {
+    const container = this.element.querySelector('.page-item__body')! as HTMLElement;
+    child.attachTo(container);
+  }
+}
+export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
   constructor() {
     // 여기서 super는 extends한 BaseComponent에 constructor. => htmlString 인자가 필요함
-    super('<ul class="page">This is PageComponent!</ul>');
+    super('<ul class="page"></ul>');
+  }
+
+  /**
+   * 인자로 받는 Component를 pageItemComponent로 감싸는 method
+   */
+  addChild(section: Component) {
+    const item = new PageItemComponent();
+    item.addChild(section);
+    item.attachTo(this.element, 'beforeend');
   }
 }
